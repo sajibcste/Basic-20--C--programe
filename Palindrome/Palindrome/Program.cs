7ae06d446c5d2f0ee3be56fd5872adf4b154a98b
sajibcste <sajib.h371@gmail.com>
2017-10-16 15:50:37 +0600

    Plaindrome
    plaindrome

---
+++ Palindrome/Palindrome/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int reverse=0, n,a;
+            Console.WriteLine("Please Enter your digit");
+            a = int.Parse(Console.ReadLine());
+            n = a;
+            while (n != 0)
+            {
+                reverse = reverse*10;
+                reverse = reverse + n%10;
+                n = n/10;
+
+            }
+
+            if (reverse == a)
+            {
+                Console.WriteLine("This number is plaindrome");
+            }
+            else
+            {
+                Console.WriteLine("This number is not plaindrome");
+            }
+            Console.ReadKey();
+        }
+    }
+}
