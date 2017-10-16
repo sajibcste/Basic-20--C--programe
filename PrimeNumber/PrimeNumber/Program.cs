c76591fd3f2230554c336aca31756a6d957a7474
sajibcste <sajib.h371@gmail.com>
2017-10-16 16:46:23 +0600

    PrimeNumberCheck
    PrimeNumberCheck

---
+++ PrimeNumber/PrimeNumber/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x,Count;
+            Console.WriteLine("Please enter your number to check prime");
+            x = int.Parse(Console.ReadLine());
+            for (Count = 2; Count <= x - 1; Count++)
+            {
+                if (x%Count == 0)
+                {
+                    Console.WriteLine("Number is not prime");
+                    break;
+                }
+                
+            }
+            if (x == Count)
+            {
+                Console.WriteLine("Number is prime");
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
+ //if ( n >= 1 )
+ //  {
+ //     printf("First %d prime numbers are :\n",n);
+ //     printf("2\n");
+ //  }
+ 
+ //  for ( count = 2 ; count <= n ;  )
+ //  {
+ //     for ( c = 2 ; c <= i - 1 ; c++ )
+ //     {
+ //        if ( i%c == 0 )
+ //           break;
+ //     }
+ //     if ( c == i )
+ //     {
+ //        printf("%d\n",i);
+ //        count++;
+ //     }
+   //   i++;
+   //}
